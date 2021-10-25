f3a63f6db679eed5c3a69870802e13fc4974967b
refrain <liuze@spacetech.com.cn>
2021-10-25 10:18:35 +0800

    Initial commit

---
+++ src/main/java/top/refrain/studycode/StudycodeApplication.java
@@ -0,0 +1,35 @@
+package top.refrain.studycode;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootApplication
+@Slf4j
+public class StudycodeApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudycodeApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    
+    }
+
+    private void showConnection() throws SQLException{
+        log.info(dataSource.toString());
+        Connection connection = dataSource.getConnection();
+        log.info(connection.toString());
+        connection.close();
+    }
+}
---
+++ src/test/java/top/refrain/studycode/StudycodeApplicationTests.java
@@ -0,0 +1,13 @@
+package top.refrain.studycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudycodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
